7c3a14031fe4760445178957a4e05067c889c1dc
YellowSmy <fprh2005@naver.com>
2025-02-15 16:28:25 +0900

    Style: nav바, home css 적용

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    //reply-btn
     const replyButtons = document.querySelectorAll('.reply-button');
 
     replyButtons.forEach(button => {
@@ -12,5 +13,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
+
+//toggle function
+function toggle(id) {
+    const contentDiv = document.getElementById(id);
+
+    if (contentDiv.style.display == "none") {
+        contentDiv.style.display = "block";
+    } else {
+        contentDiv.style.display = "none";
+    }
+}
+
 
 
